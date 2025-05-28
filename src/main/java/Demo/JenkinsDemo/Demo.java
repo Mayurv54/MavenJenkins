aaa003c0e3dc28ce050c62f23391d973ab348ffb
Mayur54 <mayurvelani54@gmail.com>
2025-05-28 09:12:30 +0530

    sum added

---
+++ src/main/java/Demo/JenkinsDemo/Demo.java
@@ -0,0 +1,16 @@
+package Demo.JenkinsDemo;
+
+public class Demo {
+	int sum(int a , int b) {
+		return a+b;
+	}
+	
+
+	public static void main(String[] args) {
+		Demo d =new Demo();
+		System.out.println("sum is : "+d.sum(10, 20));
+		
+
+	}
+
+}
