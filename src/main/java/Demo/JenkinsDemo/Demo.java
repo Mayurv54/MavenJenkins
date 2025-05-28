d0f82b2db54b0e95d5e01cf13ece8254abbf68e1
Mayur54 <mayurvelani54@gmail.com>
2025-05-28 09:58:56 +0530

    Diff added

--- src/main/java/Demo/JenkinsDemo/Demo.java
+++ src/main/java/Demo/JenkinsDemo/Demo.java
@@ -4,11 +4,15 @@ public class Demo {
 	int sum(int a , int b) {
 		return a+b;
 	}
+	int diff(int x , int y ) {
+		return x-y;
+	}
 	
 
 	public static void main(String[] args) {
 		Demo d =new Demo();
 		System.out.println("sum is : "+d.sum(10, 20));
+		System.out.println("Diff is : "+d.diff(44, 22));
 		
 
 	}
